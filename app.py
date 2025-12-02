c8bb99955f0c51d98faff9a1a682449fbc62162f
jprinaINCO <jprina@incosoluciones.com>
2025-12-01 23:06:05 -0300

    7

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hola desde EKS"
+    return "Hola desde EKS 7"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
